a0db972644c705f6fb0a9c5d0cd5fa1dcf0aa02c
cristiano alexandre de faria <cristianoaf81@gmail.com>
2021-06-05 18:38:55 -0300

    adição de pacote para deploy no github pages

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -8,7 +8,7 @@ import Stock from '../shared/models/stock-model';
 export class DashboardService {
 
   private readonly baseUrl: string = 'http://localhost:8080/bootcamp';
-  private readonly remoteUrl = 'https://bootcamp-dio-san-cristiano.herokuapp.com/bootcamp';
+  private readonly remoteUrl = 'https://bootcamp-dio-san-cristiano.herokuapp.com/bootcamp/';
 
   constructor(private http: HttpClient) { }
 
